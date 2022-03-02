7a093e7286002a61d98b7a372e0773d8cac030f0
Michael Salerno <msalerno1993@gmail.com>
2022-03-02 01:37:17 -0500

    enabled search and clearing of state

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import Footer from './Components/Layout/Footer';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import NotFound from './Pages/NotFound';
+import { GithubProvider } from './Context/Github/GithubContext';
 
 
 function App() {
   return (
-    <Router >
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
+    <GithubProvider>
+      <Router >
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
+          <Footer />
 
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
